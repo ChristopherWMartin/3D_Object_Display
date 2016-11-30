5a5ea5ecd69970b48fe50afa8e08bd5bb079d651
christopherwmartin <cmarti14@artic.edu>
2016-11-30 11:10:42 -0600

    cleaned code and directory

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -18,19 +18,19 @@ void ofApp::setup(){
     //3dObject_1.setup();
 
     // work in Blender to get image wraps working
-    model.loadModel("3DModel_wrapped3.dae");
+    model.loadModel("1.dae");
     model.setRotation(0, 0, 0, 0, 0);
     model.setScale(0.9, 0.9, 0.9);
     model.setPosition(50, 50, 0);
     mesh = model.getMesh(0);
 
-    model2.loadModel("3DModel2.3ds");
+    model2.loadModel("2.3ds");
     model2.setRotation(0, 90, 1, 1, 0);
     model2.setScale(0.9, 0.9, 0.9);
     model2.setPosition(50, 50, 0);
     mesh2 = model2.getMesh(0);
 
-    model3.loadModel("3DModel3.dae");
+    model3.loadModel("3.dae");
     model3.setRotation(0, 90, 1, 2, 0);
     model3.setScale(0.9, 0.9, 0.9);
     model3.setPosition(900, 300, 900);
@@ -39,8 +39,8 @@ void ofApp::setup(){
     videoPlayer.loadMovie("video.mp4");
     videoPlayer.play();
 
-    tex.load("pattern_idea.png");
-    tex2.load("carrepeatscreenshot4.png");
+    tex.load("1.png");
+    tex2.load("2.png");
     //tex3.load("");
 
     light.enable();
@@ -71,26 +71,26 @@ void ofApp::update(){
 
 void ofApp::draw(){
 
-    //tracker.draw();
+    easyCam.setDistance(400);
     easyCam.begin();
 
     //videoPlayer.draw(ofGetWidth()/2, ofGetHeight()/2);
 
     if(tracker.getFound()) {
 
-      //ofBackground(0, 255, 200);
+        //tracker.draw();
+
+        //ofBackground(videoPlayer.draw(0,0));
 
         ofPolyline leftEye = tracker.getImageFeature(ofxFaceTracker::LEFT_EYE);
         ofVec2f leftCenter = leftEye.getBoundingBox().getCenter();
         float rawLeftEye_x = ofMap(leftCenter.x, 0, 500, 0, 800, true);
         float rawLeftEye_y = ofMap(leftCenter.y, 0, 500, 0, 800, true);
-        float rawLrackerScale = ofMap(tracker.getScale(), 0, 8, 0, 400, true);
+        float rawLrackerScale = ofMap(tracker.getScale(), 0, 8, 50, 200, true);
 
         leftEye_x = (alpha) * leftEye_x + (1 - alpha) * rawLeftEye_x;
         leftEye_y = (alpha) * leftEye_y + (1 - alpha) * rawLeftEye_y;
         trackerScale = (alpha) * trackerScale + (1 - alpha) * rawLrackerScale;
-
-
 
         //3dObject_1.draw();
 
